be7732d060d8a05d424dcb591c24b6be7e221606
JCutting1972 <cutt.joe1972@gmail.com>
2023-05-04 03:56:41 -0400

    FinalChanges

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,19 @@
+
+import json
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import os
+
+
+
+api_key = ['apikFgvhJhrvyxG09ko05FnhABEiTGfQXW2jlKS0bjTMcLryey']
+url = ['https://api.au-syd.assistant.watson.cloud.ibm.com/instances/cc131269-1e43-4679-b902-462cbac0c5e0l']
+model_id = 'en-it'
+text_to_translate = 'blue'
+language translator.set_service_url(https://api.au-syd.assistant.watson.cloud.ibm.com/instances/cc131269-1e43-4679-b902-462cbac0c5e0)
+
+def EnglishToFrench(english_text):
+    translation = language_translator.text_to_translate,model_id = model_id),get result()
+    return french_text
+    print(json.dumps(translation. indent=2, ensure_ascii=False)
+    
