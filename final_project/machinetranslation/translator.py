6974ede787bf0e89080eecfa816cb210a41234d8
JCutting1972 <cutt.joe1972@gmail.com>
2023-05-06 23:11:33 -0400

    jo!

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -2,14 +2,15 @@
 import json
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 
 
 
-api_key = ('kFgvhJhrvFgvhJhrvyxG09ko05FnhABEiTGfQXW2jlKS0bjTMcLryyxG09ko05FnhABEiTGfQXW2jlKS0bjTMcLry')
-url = ['https://api.au-syd.assistant.watson.cloud.ibm.com/instances/cc131269-1e43-4679-b902-462cbac0c5e0l']
-model_id = 'en-it'
-text_to_translate = 'blue'
+
+api_key = ('iOPVcUJiht0UA4vvWs-b1RttqQ2aFLylFb3Dxzu0gsuA')
+api_url = ('https://api.au-syd.language-translator.watson.cloud.ibm.com/instances/999bf2d6-20bf-4bff-b55a-3c47f0eae0a2')
+model_id = 'en-fr'
+text_to_translate = 'snail'
+
 
 
 
@@ -18,21 +19,16 @@ text_to_translate = 'blue'
 
 authenticator = IAMAuthenticator(api_key)
 language_translator = LanguageTranslatorV3(
-    version=('2018-05-01'),translation = language_translator.translate(
-    text_to_translate = 'lobster'
-    
-    authenticator=authenticator
-
+    version = '2018-05-01',
+    authenticator = authenticator
 )
 
-language_translator.set_service_url('https://api.au-syd.assistant.watson.cloud.ibm.com/instances/cc131269-1e43-4679-b902-462cbac0c5e0')
+
+language_translator.set_service_url(api_url)
 
 translation = language_translator.translate(
-    text=text_to_translate,
-    'en-it').get_result()
 
-
+text = text_to_translate,model_id = model_id).get_result()
 #
 # return french_text
 print(json.dumps(translation, indent=2, ensure_ascii=False))
-
