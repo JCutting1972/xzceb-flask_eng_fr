da75a3e08dd951dbc205514150c117f93eef3f3c
JCutting1972 <cutt.joe1972@gmail.com>
2023-05-07 17:53:49 -0400

    www

---
+++ final_project/machinetranslation/tests.py
@@ -0,0 +1,11 @@
+import unittest
+
+from translator import EnglishToFrench
+
+class TestTanslator(unittest.TestCase):
+   def test_EnglishToFrench(self):
+            self.assertEqual(EnglishToText(('blue'),'bleu')
+
+if __name__ == '__main__':
+
+    unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -4,25 +4,37 @@ from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 api_key = ('iOPVcUJiht0UA4vvWs-b1RttqQ2aFLylFb3Dxzu0gsuA')
 api_url = ('https://api.au-syd.language-translator.watson.cloud.ibm.com/instances/999bf2d6-20bf-4bff-b55a-3c47f0eae0a2')
-model_id = 'fr-en'
+model_id = 'en-fr'
 english_text = 'error'
 french_text = 'error'
+translation = 'error'
 authenticator = IAMAuthenticator(api_key)
 language_translator = LanguageTranslatorV3(
     version = '2018-05-01',
     authenticator = authenticator )
 
-def FrenchToEnglish(french_text):
+def EnglishToFrench(english_text):
     
-    englishtext = input('Please enter text to translate: ')    
+    english_text = input('Please enter text to translate: ')    
     language_translator.set_service_url(api_url)
     translation = language_translator.translate(
-    text = frenchtext,model_id = model_id).get_result()
-    englishtext = translation    
-    print(json.dumps(translation, indent=2, ensure_ascii=False)) 
-    return englishtext
+    text = english_text,model_id = model_id).get_result()
 
-FrenchToEnglish(english_text)
+
+   #
+   # 
+    print(json.dumps(translation, indent=2, ensure_ascii=False)) 
+    french_text = translation   
+    return french_text
+    print(translation)
+    print(french_text)
+
+EnglishToFrench(english_text)
+#
+# 
+# print(french_text)
+
+
 
 
 
