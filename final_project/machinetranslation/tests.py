28a7e6e834f7553c2f4ecf9a3a39ef289ff3498e
JCutting1972 <cutt.joe1972@gmail.com>
2023-05-07 22:04:42 -0400

    ad

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,11 +1,11 @@
 import unittest
 
-from translator import EnglishToFrench
+from translator import EnglishToFrench(english_text,french_text)
 
 class TestTanslator(unittest.TestCase):
    def test_EnglishToFrench(self):
-            self.assertEqual(EnglishToText(('blue'),'bleu')
+        self.assertTrue(EnglishToText((blue),bleu)
+            
 
-if __name__ == '__main__':
-
+if __name__ =='__main__':
     unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,5 +1,6 @@
 
 import json
+import requests
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 api_key = ('iOPVcUJiht0UA4vvWs-b1RttqQ2aFLylFb3Dxzu0gsuA')
@@ -7,7 +8,7 @@ api_url = ('https://api.au-syd.language-translator.watson.cloud.ibm.com/instance
 model_id = 'en-fr'
 english_text = 'error'
 french_text = 'error'
-translation = 'error'
+
 authenticator = IAMAuthenticator(api_key)
 language_translator = LanguageTranslatorV3(
     version = '2018-05-01',
@@ -15,24 +16,49 @@ language_translator = LanguageTranslatorV3(
 
 def EnglishToFrench(english_text):
     
-    english_text = input('Please enter text to translate: ')    
-    language_translator.set_service_url(api_url)
+    #english_text = input('Please enter text to translate: ')    
+    
+    #language_translator.set_service_url(api_url)
     translation = language_translator.translate(
     text = english_text,model_id = model_id).get_result()
 
 
    #
    # 
-    print(json.dumps(translation, indent=2, ensure_ascii=False)) 
-    french_text = translation   
+   # print(json.dumps(translation, indent=2, ensure_ascii=False)) 
+      
+    json_string = json.dumps(translation, indent=2, ensure_ascii=False)
+    print(json_string)
+# Get dummy data using an API
+#res = requests.get("http://dummy.restapiexample.com/api/v1/employees")
+ 
+  #Convert data to dict
+    
+    
+   
+    
+    french_text =  json_string
     return french_text
-    print(translation)
-    print(french_text)
+    
+   #
+   # 
+   #  return french_text
+    print (french_text)
+ 
+# Convert dict to string
+   # Dict = json.dumps(translation) 
+   # print(french_text)
+    #print(type(french_text))
+
+
+
+
+    return french_text
+    
+    
 
 EnglishToFrench(english_text)
-#
-# 
-# print(french_text)
+
 
 
 
