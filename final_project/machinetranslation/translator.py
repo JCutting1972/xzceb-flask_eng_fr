ef787476f7061ba09235ae7ae0635749522ba553
JCutting1972 <cutt.joe1972@gmail.com>
2023-05-11 03:50:21 -0400

    yu

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -7,31 +7,42 @@ import os
 
 api_key = ('iOPVcUJiht0UA4vvWs-b1RttqQ2aFLylFb3Dxzu0gsuA')
 api_url = ('https://api.au-syd.language-translator.watson.cloud.ibm.com/instances/999bf2d6-20bf-4bff-b55a-3c47f0eae0a2')
-languages = 'error'
-english_text = 'error'
-french_text = 'error'
+languages    =  'error'
+english_text =  'error'
+french_text  =  'error'
+english_txt  =  'error'
+french_txt   =  'error'
 
 authenticator = IAMAuthenticator(api_key)
 language_translator = LanguageTranslatorV3(
     version = '2018-05-01',
-    authenticator = authenticator )
+    authenticator = authenticator)
 
-def EnglishToFrench(english_text,languages):
-    
-        
-    
+def english_to_french(english_text,languages):
+         
     language_translator.set_service_url(api_url)
     translation = language_translator.translate(
-    text = english_text,model_id = languages).get_result()
-   
-    
-      
+    text = english_text, model_id = languages).get_result()
+        
     json_string = json.dumps(translation)
     parsed_response = json.loads(json_string)
-    french_text = parsed_response['translations'][0]['translation']
+    french_text = (parsed_response['translations'][0]['translation'])
         
     return french_text
 
+def french_to_english(french_txt,languages):          
+    
+    language_translator.set_service_url(api_url)
+    translation = language_translator.translate(
+    text = french_txt, model_id = languages).get_result()
+        
+    json_string = json.dumps(translation)
+   
+    parsed_response = json.loads(json_string)
+    english_txt = (parsed_response['translations'][0]['translation'])
+        
+    return english_txt
+
 
 
 
@@ -40,7 +51,7 @@ def EnglishToFrench(english_text,languages):
     
     
 
-#EnglishToFrench(english_text,model_id)
+
 
 
 
