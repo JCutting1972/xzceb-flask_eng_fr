656b1abece755a0e0abe2fb592e641e4a5f00bd0
JCutting1972 <cutt.joe1972@gmail.com>
2023-05-24 04:46:44 -0400

    ttt

--- final_project/machinetranslation/server.py
+++ final_project/machinetranslation/server.py
@@ -3,25 +3,13 @@
 from flask import Flask, render_template, request
 import json
 
-import json
+#import jsonify
 #import requests
-from ibm_watson import LanguageTranslatorV3
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
+#from ibm_watson import LanguageTranslatorV3
+#from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+#import os
 
 
-def pi_key = ('iOPVcUJiht0UA4vvWs-b1RttqQ2aFLylFb3Dxzu0gsuA')
-api_url = ('https://api.au-syd.language-translator.watson.cloud.ibm.com/instances/999bf2d6-20bf-4bff-b55a-3c47f0eae0a2')
-languages    =  'error'
-english_text =  'error'
-french_text  =  'error'
-english_txt  =  'error'
-french_txt   =  'error'
-
-authenticator = IAMAuthenticator(api_key)
-language_translator = LanguageTranslatorV3(
-    version = '2018-05-01',
-    authenticator = authenticator)
 
 
 app = Flask("Web Translator")
@@ -32,9 +20,41 @@ def processs_data():
     data = request.get_json()
     english_text = data['input1']
     languages = data['input2']
-    french_text = english_to_French(english_text,languages)
 
-    return jsonify({'result':french_text})
+    global french_text
+
+    french_text = english_to_french(english_text, languages)
+
+    return jsonify(result = french_text)
+    
+    
+    
+    #english_text 
+    
+    #french_text = json_string
+    
+    #french_text = (parsed_response['translations'][0]['translation'])
+
+    #french_text = 
+        
+    #return french_text
+
+
+    #french_text = english_to_French(english_text,languages)
+    
+    
+
+
+@app.route('/retrieve', methods=['GET'])
+def retrieve_data():
+    global french_text
+    if french_text is not None:
+
+        return jsonify(result = french_text)
+
+    else:
+
+        return jsonify(error = 'no data')
  #   english_text = request.args.get('Text to Translate')
    #textToTranslate = request.args.get('textToTranslate')
     # Write your code here
@@ -60,7 +80,7 @@ def index():
     # Write the code to render template
 
 
-def english_to_french(english_text,languages):
+#def english_to_french(english_text,languages):
          
     #language_translator.set_service_url(api_url)
     #translation = language_translator.translate(
@@ -72,9 +92,9 @@ def english_to_french(english_text,languages):
     
     #french_text = (parsed_response['translations'][0]['translation'])
 
-    french_text = 
+   # french_text = 
         
-    return french_text
+    #return french_text
 
     
 
