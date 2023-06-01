257235c424904f6315ac2015b536b061cd24862a
JCutting1972 <cutt.joe1972@gmail.com>
2023-06-01 03:23:37 -0400

    nnbh

--- final_project/machinetranslation/server.py
+++ final_project/machinetranslation/server.py
@@ -1,15 +1,27 @@
-from tests import tests
-from flask import flask, requests, render_templates
+from flask import Flask, render_template
+import json
+from translator import english_to_french
+
+app = Flask("Translator")
+books = []
+
+@app.route("/english_to_text")
+def english_to_french(english_text):
+    books.append({"translate to french": french_text})
+    return "%s by %s appended" %(french_text)
 
 
-app = (__name__)
-
-@app.route(/)
-
-def index_function():
-    return = render_templates(index.html)
-    
-def english_function(/english_to_french)
 
 
-def french_function(/french_to_english)
+
+@app.route("/english_to_french")
+def englih_to_french(english_text):
+    print(french_text)
+
+@app.route("/")
+def homepage():
+    return render_template("index.html")
+
+if __name__=="__main__":
+    app.run(debug=True) 
+    # When no port is specified, starts at default port 5000
--- final_project/machinetranslation/static/mywebscript.js
+++ final_project/machinetranslation/static/mywebscript.js
@@ -1,26 +1,37 @@
-let translateToFrench = ()=>{
-    textToTranslate = document.getElementById("textToTranslate").value;
+//This is a javascript code to be used in the HTML page through static rendering
+
+let english_to_text = ()=>{
+    bookname = document.getElementById(english_text).value;
+    
 
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("translated_text").innerHTML = xhttp.responseText;
+            document.getElementById("updatemessage").innerHTML = xhttp.responseText;
+            setTimeout(function(){ 
+                document.getElementById("updatemessage").innerHTML = "<br/>"; 
+            }, 1000);
+            getBook();
         }
     };
-    xhttp.open("GET", "englishToFrench?textToTranslate"+"="+textToTranslate, true);
+    xhttp.open("GET", "english_text/", true);
     xhttp.send();
 }
 
-let translateToEnglish = ()=>{
-    textToTranslate = document.getElementById("textToTranslate").value;
-
+let english_to_fench = ()=>{
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("translated_text").innerHTML = xhttp.responseText;
+            let books = JSON.parse(xhttp.french_text);
+            retstr = "<table class='table table-striped' style='width:100%;'><tr><th>English Text</th></tr>"
+            books.forEach(element => {
+                retstr+="<tr><td>"+element['english_text']+"<tr></tr>"
+            }); 
+            retstr+="</table>"
+
+            
         }
     };
-    xhttp.open("GET", "frenchToEnglish?textToTranslate"+"="+textToTranslate, true);
+    xhttp.open("GET", print(french_text), true);
     xhttp.send();
 }
-
